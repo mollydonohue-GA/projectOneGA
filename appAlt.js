65efde037b0b1f8a89de9e6d8e1b2c768fc8e744
Molly Donohue <mollykennan@gmail.com>
2015-10-15 15:54:41 -0400

    improves game reset feature - page refresh

--- appAlt.js
+++ appAlt.js
@@ -5,18 +5,23 @@ $(document).ready(function() {
 var spot = $(".spot");
 console.log(spot);
 
-var button = $(".reset");
-console.log(button);
+var buttonReset = $(".reset");
+console.log(buttonReset);
 
 // var spot1 = $("")
 
 $(spot).click(function(){
-	spot.css('background-color', 'blue');
+	spot.css('background-color', 'yellow');
 	// spot.css('cursor', 'none');
 })
 
-$(button).click(function(){
-	spot.css('background-color', 'white');
+
+
+
+$(buttonReset).click(function(){
+	location.reload();
 })
+
+
 
 });
