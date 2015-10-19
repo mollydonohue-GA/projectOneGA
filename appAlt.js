bba0e47864079802a55ab085caac12898844b159
Molly Donohue <mollykennan@gmail.com>
2015-10-19 09:12:15 -0400

    final

--- appAlt.js
+++ appAlt.js
@@ -1,6 +1,8 @@
 console.log("running");
 $(document).ready(function() {
 
+var turnCount = 0;
+
 var turn = true; 
 
 var spot = $(".spot");
@@ -9,32 +11,39 @@ console.log(spot);
 var buttonReset = $(".reset");
 console.log(buttonReset);
 
-var clicked = $(".clicked");
-console.log(clicked);
-
-
 
 $(spot).click(function(){
-	if ($(this).hasClass('.clicked')){
-		turn = turn;
-	}else {
-
-		 if (turn){
-			$(this).css('background-color', 'yellow');
-			// $(this).css('cursor', 'none');
-			$(this).addClass('.clicked'); 
-			turn = false;
-		} else {
-			$(this).css('background-color', 'black');
-			// $(this).css('cursor', 'none');
-			$(this).addClass('.clicked'); 	
-			turn = true;
-		}
-	}		
+	if ($(this).hasClass('row5')){
+		($(spot).addClass('row5'));	
+		// turnCount ++;
+		if ($(this).hasClass('.clicked')){
+			turn = turn;
+			// turnCount ++;
+		}else {
+			if (turn){
+				$(this).css('background-color', 'yellow');
+				// $(this).css('cursor', 'none');
+				$(this).addClass('.clicked'); 
+				$(this).addClass('.yellow');
+				turn = false;
+				turnCount ++;
+			} else {
+				$(this).css('background-color', 'black');
+				// $(this).css('cursor', 'none');
+				$(this).addClass('.clicked'); 	
+				$(this).addClass('.black');
+				turn = true;
+				turnCount ++;
+				console.log(turnCount);
+			} 
+		}	
+				
+	} 		
 })
 
-
-
+if (turnCount === 8){
+	alert("yellow wins!");
+}
 
 $(buttonReset).click(function(){
 	location.reload();
