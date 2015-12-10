9197050904d5771554a678e68b09a0ace10304b8
Molly Donohue <mollykennan@gmail.com>
2015-12-10 11:25:54 -0500

    connect updates

--- ConnectFOUR/appAlt.js
+++ ConnectFOUR/appAlt.js
@@ -11,39 +11,73 @@ console.log(spot);
 var buttonReset = $(".reset");
 console.log(buttonReset);
 
+var spotClicked = function(){
+	
+	var spotCheck = $(this).parent().children();
 
-$(spot).click(function(){
-	if ($(this).hasClass('row5')){
-		($(spot).addClass('row5'));	
-		// turnCount ++;
-		if ($(this).hasClass('.clicked')){
-			turn = turn;
-			// turnCount ++;
-		}else {
-			if (turn){
-				$(this).css('background-color', 'yellow');
-				// $(this).css('cursor', 'none');
-				$(this).addClass('.clicked'); 
-				$(this).addClass('.yellow');
+	for (var i = spotCheck.length - 1; i >= 0; i--) {
+		if (!($(spotCheck[i]).hasClass('clicked'))){
+
+			if (turn) {
+				$(spotCheck[i]).css('background-color', 'yellow');
+				$(spotCheck[i]).addClass('clicked'); 
+				$(spotCheck[i]).addClass('yellow');
 				turn = false;
 				turnCount ++;
 			} else {
-				$(this).css('background-color', 'black');
-				// $(this).css('cursor', 'none');
-				$(this).addClass('.clicked'); 	
-				$(this).addClass('.black');
+				$(spotCheck[i]).css('background-color', 'black');
+				$(spotCheck[i]).addClass('clicked'); 	
+				$(spotCheck[i]).addClass('black'); 
 				turn = true;
 				turnCount ++;
-				console.log(turnCount);
-			} 
-		}	
+			}
+
+			// console.log("free space");
+			// console.log(spotCheck[i]);
+			
+			// $(spotCheck[i]).addClass('clicked'); 
+			// if ($(this).hasClass('.clicked')){
+			// turn = turn;
+			// // turnCount ++;
+			// }else {
+			// 	if (turn){
+			// 		$(this).css('background-color', 'yellow');
+			// 		// $(this).css('cursor', 'none');
+			// 		$(this).addClass('.clicked'); 
+			// 		$(this).addClass('.yellow');
+			// 		turn = false;
+			// 		turnCount ++;
+			// 	} else {
+			// 		$(this).css('background-color', 'black');
+			// 		// $(this).css('cursor', 'none');
+			// 		$(this).addClass('.clicked'); 	
+			// 		$(this).addClass('.black');
+			// 		turn = true;
+			// 		turnCount ++;
+			// 		console.log(turnCount);
+			// 	} 
+			// }
+			break;	
+		} else {
+			console.log("nah");
+		}
+	};
+
+	if ($(this).hasClass('row5')){
+		($(spot).addClass('row5'));	
+		// turnCount ++;
+		
 				
 	} 
 	// if ($(this).hasClass('row2')){
 	// alert("yellow wins!");
 	// ($(spot).css('background-color', 'yellow'));
 	// }		
-})
+};
+
+$(spot).click(spotClicked);
+
+
 
 
 
